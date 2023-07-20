5444d8f12b522c5027753ef4e5602276abc2d0c8
Brian Loh <bloh@Brians-MacBook-Pro-4.local>
2023-07-19 17:00:29 -0700

    datasource singleton

--- Datasource.java
+++ Datasource.java
@@ -1,5 +1,26 @@
-//holds current world, correct world, 
+import java.util.LinkedList;
 
 public class Datasource {
-    
+    private static Datasource datasource = null;
+    private LinkedList<Cell> cellList = new LinkedList<Cell>();
+
+    private Datasource(){}
+
+    public static Datasource getInstance()
+    {
+        if (datasource == null)
+            datasource = new Datasource();
+  
+        return datasource;
+    }
+
+    public LinkedList<Cell> getCellList(){
+        return this.cellList;
+    }
+
+    public void setCellList(LinkedList<Cell> l){
+        this.cellList = l;
+    }
+
+
 }
