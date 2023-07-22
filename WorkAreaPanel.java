74d277dac885818d458796f176f1212bf7f9acdf
Alex Leung <alexleung@Alexs-MacBook-Air-5.local>
2023-07-22 15:51:52 -0700

    update

--- WorkAreaPanel.java
+++ WorkAreaPanel.java
@@ -1,14 +1,19 @@
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import javax.swing.JPanel;
+
+import javax.swing.border.LineBorder;
+
 import java.awt.Graphics;
 
 public class WorkAreaPanel extends JPanel implements MouseListener, MouseMotionListener{
 
+    @Override
     public void paintComponent(Graphics g){
-
-
-
+        setBackground(Color.WHITE);
+        setPreferredSize(new Dimension(600, 700));
+        setBorder(new LineBorder(Color.BLACK,1));
+        super.paintComponent(g);
     }
 
 
