e0911312aa5ab0abd9016b9bfebbb4ba6b1ed85c
Brian Loh <brian825@gmail.com>
2023-07-26 17:30:41 -0700

    loops

--- Loop.java
+++ Loop.java
@@ -1,6 +1,24 @@
+import java.util.LinkedList;
+
 public class Loop{
 
-    private int totalLoops;
-    private int loopCounter;
+    private int loops;
+    private LinkedList<Block> blockList = new LinkedList<Block>();
+
+    public Loop(int loops){
+        this.loops = loops;
+    }
+
+    public void addBlock(Block b){
+        blockList.add(b);
+    }
+
+    public LinkedList<Block> getBlockList(){
+        return this.blockList;
+    }
+
+    public int getLoops(){
+        return this.loops;
+    }
 
 }
