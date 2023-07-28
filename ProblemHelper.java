133001c14ce569acd6cf209f13fd63637b3f2a7d
Stanley <skha01@calpoly.edu>
2023-07-27 17:58:25 -0700

    created more levels for spider world

--- ProblemHelper.java
+++ ProblemHelper.java
@@ -3,9 +3,11 @@ import java.util.LinkedList;
 
 public class ProblemHelper {
     private Spider spider;
+  //  private World world;
 
     public ProblemHelper(Spider spider){
         this.spider = spider;
+        //this.world = world;
     }
 
     public LinkedList<Cell> getLevel(int level) {
@@ -15,6 +17,7 @@ public class ProblemHelper {
         }   
 
         if(level == 1){
+            //world.setMatrixSize(5, 5);
             spider.setDirection("north");
             Cell cell = cells.get(6);
             cell.setDiamond(true, Color.RED);
@@ -22,7 +25,6 @@ public class ProblemHelper {
             cell.setDiamond(true, Color.BLUE);
             cell = cells.get(11);
             cell.setDiamond(true, Color.GREEN);
-            //cell = cells.get(16);
             spider.setPosition(3, 1);
             cell.setSpider(true);
         }
@@ -39,27 +41,61 @@ public class ProblemHelper {
         }
 
         else if(level == 3) {
-
+            spider.setDirection("north");
+            Cell cell = cells.get(6);
+            cell.setDiamond(true, Color.RED);
+            cell = cells.get(7);
+            cell.setDiamond(true, Color.BLUE);
+            cell = cells.get(8);
+            cell.setDiamond(true, Color.RED);
+            cell = cells.get(12);
+            cell.setDiamond(true, Color.BLUE);
+            spider.setPosition(3, 2);
             
         }
         
         else if(level == 4) {
-
-            
+            spider.setDirection("south");
+            Cell cell = cells.get(6);
+            cell.setDiamond(true, Color.BLUE);
+            cell = cells.get(8);
+            cell.setDiamond(true, Color.BLUE);
+            spider.setPosition(0, 2);
         }
 
         else if(level == 5) {
-
-            
+            spider.setDirection("south");
+            Cell cell = cells.get(19);
+            cell.setDiamond(true, Color.BLUE);
+            cell = cells.get(22);
+            cell.setDiamond(true, Color.GREEN);
+            cell = cells.get(24);
+            cell.setDiamond(true, Color.GREEN);
+            spider.setPosition(2, 4);
         }
 
         else if(level == 6) {
+            spider.setDirection("east");
+            spider.setPosition(0, 0);
+            Cell cell = cells.get(1);
+            cell.setDiamond(true, Color.RED);
+            cell = cells.get(3);
+            cell.setDiamond(true, Color.GREEN);
+            cell = cells.get(6);
+            cell.setDiamond(true, Color.RED);
+            cell = cells.get(8);
+            cell.setDiamond(true, Color.GREEN);
+            cell = cells.get(11);
+            cell.setDiamond(true, Color.BLUE);
+            cell = cells.get(12);
+            cell.setDiamond(true, Color.BLUE);
+            cell = cells.get(13);
+            cell.setDiamond(true, Color.BLUE);
 
-            
         }
 
         else if(level == 7) {
-
+            spider.setDirection("east");
             
         }
 
